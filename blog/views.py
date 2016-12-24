715a354741ae92c1be3278407675ea19e8f99f8c
webtech2k16 <s.n.khalil@student.tue.nl>
2016-12-24 21:49:51 +0100

    Update views.py

--- blog/views.py
+++ blog/views.py
@@ -7,7 +7,7 @@ from django.shortcuts import render, get_object_or_404
 
 def post_list(request):
     #QuerySet to order by posted date
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
 
 def post_detail(request, pk):
